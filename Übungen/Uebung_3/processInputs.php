f13dd0d4d08db4d410aa8bae0145c3f72e870718
LG-Cologne <leon.jarosch@web.de>
2022-04-05 12:14:19 +0200

    Ausgabe der Daten II done

--- Übungen/Uebung_3/processInputs.php
+++ Übungen/Uebung_3/processInputs.php
@@ -1,3 +1,24 @@
 <?php
-print_r($_POST);
-print_r( $_GET);
+require './mustache.php-2.14.1/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+//print_r($_POST);
+//print_r( $_GET);
+
+$template  = file_get_contents("./templates/beispiel.tpl.html");
+$mustache = new Mustache_Engine();
+
+$METHOD_TYPE = null;
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $METHOD_TYPE = $_GET;
+}else if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $METHOD_TYPE = $_POST;
+}
+
+echo $mustache ->render($template, array(
+    'title' => "WebEng-PHP",
+    'mail' => $METHOD_TYPE['mail'],
+    'password' => $METHOD_TYPE['password'],
+    'status' => $METHOD_TYPE['status']
+));
