a18dc4c374e39cb3b79063b4339d1fe0d697177b
LG-Cologne <leon.jarosch@web.de>
2022-04-05 12:51:43 +0200

    Ausgabe der Daten II refactored

--- Übungen/Uebung_3/processInputs.php
+++ Übungen/Uebung_3/processInputs.php
@@ -8,17 +8,9 @@ Mustache_Autoloader::register();
 $template  = file_get_contents("./templates/beispiel.tpl.html");
 $mustache = new Mustache_Engine();
 
-$METHOD_TYPE = null;
-
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
-    $METHOD_TYPE = $_GET;
-}else if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $METHOD_TYPE = $_POST;
-}
-
 echo $mustache ->render($template, array(
     'title' => "WebEng-PHP",
-    'mail' => $METHOD_TYPE['mail'],
-    'password' => $METHOD_TYPE['password'],
-    'status' => $METHOD_TYPE['status']
+    'mail' => $_REQUEST['mail'],
+    'password' => $_REQUEST['password'],
+    'status' => $_REQUEST['status']
 ));
