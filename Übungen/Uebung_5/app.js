782f304508c71427a1cd96db5a1709138d2b3fde
LG-Cologne <leon.jarosch@web.de>
2022-04-19 13:05:03 +0200

    Ue5 Organisation des Codes + Umstellung auf nodejs

---
+++ Übungen/Uebung_5/app.js
@@ -0,0 +1,75 @@
+const Student = require('./student')
+
+// Objekte
+const andre = new Student('Andre', 1)
+const nico = new Student('Nico', 2)
+const jonas = new Student('Jonas', 3)
+
+let students = [nico, andre, jonas]
+
+students.forEach(student => {
+    student.note++;
+    console.log(student.toString())
+})
+
+// Anonyme Funktion
+console.log('before sorting:');
+students.forEach(student => {
+    console.log(student.toString());
+});
+
+students.sort(function (a, b) {
+        return a.note - b.note;
+    }
+)
+;
+
+console.log('after sorting:');
+students.forEach(student => {
+    console.log(student.toString());
+});
+
+
+//Closures
+function createStudentFactory(note) {
+    return function (name) {
+        return new Student(name, note);
+    }
+}
+
+let studentFactory = createStudentFactory(5);
+
+let factoryStudents = [
+    studentFactory('David'),
+    studentFactory('Landon'),
+    studentFactory('Andreas'),
+];
+
+console.log('factory students:');
+factoryStudents.forEach(student => {
+    console.log(student.toString());
+});
+
+const express = require('express');
+let app = express();
+
+app.get('/student', function (req, res){
+    let sending = ""
+    students.forEach(student => {
+        sending += student.toString() + "\n"
+    })
+    res.type("text/plain")
+    res.send(sending)
+})
+
+app.get('/studentFactory', function (req, res){
+    let sending = ""
+    factoryStudents.forEach(student => {
+        sending += student.toString() + "\n"
+    })
+    res.type("text/plain")
+    res.send(sending)
+})
+app.listen(3000, function(){
+    console.log("Server is now listening to Port: 3000");
+});
