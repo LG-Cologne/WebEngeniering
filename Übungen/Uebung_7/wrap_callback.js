2afb62d319f6fa6a3fe5b436c2dba7f7d18c3177
LG-Cologne <leon.jarosch@web.de>
2022-05-03 11:27:03 +0200

    Ue7 start up

---
+++ Übungen/Uebung_7/wrap_callback.js
@@ -0,0 +1,35 @@
+function getFalafel(/* TODO: Parameter */) {
+    const falafel = "Falafel"
+    console.log(falafel + " aus dem Kühlschrank geholt")
+    // TODO: ...
+}
+
+function fryFalafel(falafel, /* TODO: Parameter */) {
+    setTimeout(function() {
+		const friedFalafel = "Frittierte " + falafel
+		console.log(falafel + " frittiert")
+		// TODO: ...
+	}, 1000);
+}
+
+function getWrap(/* TODO: Parameter */) {
+    const wrap = "Wrap"
+    console.log(wrap + " aus dem Schrank geholt")
+    // TODO: ...
+}
+
+function assembleFalafelWrap(wrap, friedFalafel,  /* TODO: Parameter */) {
+    const falafelwrap = "Falafel-Wrap"
+    console.log(friedFalafel + " in " + wrap + " gewickelt")
+    // TODO: ...
+}
+
+function prepareFalafelWrap(/* TODO: Parameter */) {
+    // TODO: Aufrufen der Zubereitungsschritte
+}
+
+function serve(meal) {
+    console.log(meal + " serviert")
+}
+
+// TODO: Zubereiten und Servieren
