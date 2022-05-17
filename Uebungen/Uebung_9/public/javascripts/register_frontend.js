c5f405d59d57407a0d9f0df900af51ea371bd153
LG-Cologne <leon.jarosch@web.de>
2022-05-17 14:57:18 +0200

    Ue9 DOM-Manipulation I 4/5

--- Uebungen/Uebung_9/public/javascripts/register_frontend.js
+++ Uebungen/Uebung_9/public/javascripts/register_frontend.js
@@ -1,6 +1,20 @@
+//Functions
+function getColorCode(note) {
+    switch (note) {
+        case '1':
+        case '2':
+            return "#5cb85c";
+        case '3':
+        case '4':
+            return "#f0ad4e";
+        case '5':
+        case '6':
+            return "#d9534f";
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
     // alert(document.getElementById('submit').textContent) 
-
 
     //HTML Content
     const submit = document.getElementById('submit');
@@ -8,25 +22,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const inputPasswort = document.getElementById('inputPasswort');
     const inputName = document.getElementById('inputName');
 
+    const form = document.querySelector('form')
+    const output = document.getElementById('output');
+
     //Eventlistener
-    document.addEventListener('change', function (){
-        note = inputNote.value;
-        submit.style.backgroundColor = getColorCode(note);
+    inputNote.addEventListener('change', function (){
+        submit.style.backgroundColor = getColorCode(inputNote.value);
     });
 
-    //Functions
-    function getColorCode(note) {
-        switch (note) {
-            case '1':
-            case '2':
-                return "#5cb85c";
-            case '3':
-            case '4':
-                return "#f0ad4e";
-            case '5':
-            case '6':
-                return "#d9534f";
+    form.addEventListener('submit', function(evt){
+        if(inputName.value === '' || inputPasswort.value === '' || inputNote.value ===''){
+            output.textContent = "Incomplete Input! You need to fill all the Input-Boxes."
+            evt.preventDefault()
         }
-    }
+    });
 });
 
