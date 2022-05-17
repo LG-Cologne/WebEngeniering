935be07deaf08fed10f02b3e7e98d35535afa694
LG-Cologne <leon.jarosch@web.de>
2022-05-17 15:53:07 +0200

    Ue9 DOM-Manipulation 5/5

--- Uebungen/Uebung_9/public/javascripts/register_frontend.js
+++ Uebungen/Uebung_9/public/javascripts/register_frontend.js
@@ -25,16 +25,58 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const form = document.querySelector('form')
     const output = document.getElementById('output');
 
+    const showAll = document.getElementById('showInput')
+
     //Eventlistener
-    inputNote.addEventListener('change', function (){
+    inputNote.addEventListener('change', function () {
         submit.style.backgroundColor = getColorCode(inputNote.value);
     });
 
-    form.addEventListener('submit', function(evt){
-        if(inputName.value === '' || inputPasswort.value === '' || inputNote.value ===''){
+    form.addEventListener('submit', function (evt) {
+        if (inputName.value === '' || inputPasswort.value === '' || inputNote.value === '') {
             output.textContent = "Incomplete Input! You need to fill all the Input-Boxes."
             evt.preventDefault()
         }
+    });
+
+    showAll.addEventListener('click', function (evt) {
+        const table = document.createElement('table') // creates table
+
+        const firstLine = document.createElement('tr') //creates row
+        const secondLine = document.createElement('tr') //creates row
+        const thirdLine = document.createElement('tr') //creates row
+
+        //Line1
+        let name = document.createElement('td') //creates table cells
+        let receivedName = document.createElement('td')
+
+        name.textContent = "Name:" //creates table cells
+        receivedName.textContent = inputName.value //creates table cells
+
+        firstLine.append(name, receivedName) // appends cell to row
+
+        //Line2
+        let password = document.createElement('td') //creates table cells
+        let receivedPassword = document.createElement('td')
+
+        password.textContent = "Password:"
+        receivedPassword.textContent = inputPasswort.value
+
+        secondLine.append(password, receivedPassword) // appends cell to row
+        
+        //Line3
+        let note = document.createElement('td') //creates table cells
+        let receivedNote = document.createElement('td')
+
+        note.textContent = "Note:"
+        receivedNote = inputNote.value
+
+        thirdLine.append(note, receivedNote) // appends cell to row
+
+        table.append(firstLine, secondLine, thirdLine)
+
+        output.replaceChildren(table)
+        evt.preventDefault()
     });
 });
 
